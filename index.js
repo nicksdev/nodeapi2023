ebda3d4725905917741d53e3a0812336284a0b7f
nickdev <nick@nickhughes.io>
2023-12-14 09:50:50 +1100

    Routes and API Functions

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes/routes');
 
 const app = express();
 
 app.use(express.json());
+app.use('/api', routes);
 
 app.listen(3000, () => {
     console.log('Server Started at ${3000}')
