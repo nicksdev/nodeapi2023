f3da066fa8b9433bdb96d60472db59a8e1a9a597
nickdev <nick@nickhughes.io>
2023-12-18 09:02:40 +1100

    Data Model

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Model = require('../models/model');
+
 
 module.exports = router;
 
@@ -30,3 +32,19 @@ router.delete('/delete/:id', (req, res) => {
     res.send('Delete by ID API')
 })
 
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+
+    }
+    catch (error){
+        res.status(400).json({message: error.message})
+    }
+
+})
